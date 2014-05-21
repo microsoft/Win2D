17c1a806132fad5b0ea05db45516538a771e5f15
Damyan Pepper <damyanp@microsoft.com>
2014-05-20 21:34:46 -0700

    Convert to universal projects (Windows & WindowsPhone)

--- winrt/test.internal/unittest1.cpp
+++ winrt/test.internal/unittest1.cpp
@@ -7,6 +7,12 @@ namespace winrttest
 	TEST_CLASS(UnitTest1)
 	{
 	public:
-		
+        TEST_METHOD(PlaceholderInternalTest)
+        {
+            //
+            // This test exists as a placeholder so we can exercise that
+            // internal tests do, in fact, work.
+            //
+        }
 	};
 }
