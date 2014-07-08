e36e7ac54e89586a8000d97d18d22d17d4256945
Shawn Hargreaves <shawnhar@microsoft.com>
2014-07-08 12:52:14 -0700

    Add copyright tool for automatically validating that all source files include the correct copyright banner

---
+++ tools/copyright/CommandLineOptions.cs
@@ -0,0 +1,151 @@
+// Copyright (c) Microsoft Corporation. All rights reserved.
+//
+// Licensed under the Apache License, Version 2.0 (the "License"); you may
+// not use these files except in compliance with the License. You may obtain
+// a copy of the License at http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+// WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+// License for the specific language governing permissions and limitations
+// under the License.
+
+using System;
+using System.IO;
+using System.Linq;
+using System.Collections.Generic;
+
+namespace Copyright
+{
+    public class CommandLineOptions
+    {
+        // Text file containing the copyright banner.
+        [CommandLineParser.Required]
+        public string CopyrightFile;
+
+        // Optionally specify a file containing previous copyright banner,
+        // in which case we will only overwrite things that exactly match this.
+        public string PreviousCopyright;
+
+        // File extensions to check for copyright banners (optionally semicolon separated).
+        public List<string> Ext = new List<string>();
+
+        // File extensions to skip (optionally semicolon separated).
+        public List<string> SkipExt = new List<string>();
+
+        // Directories to skip entirely (optionally semicolon separated).
+        public List<string> SkipDir = new List<string>();
+
+        // Root folder for the scan.
+        public string RootDir;
+
+        // Other options.
+        public bool Validate;
+        public bool Verbose;
+
+
+        // Internal state: private so not settable from the commandline.
+        private List<string> unknownExtensions = new List<string>();
+
+        public int UnknownExtensionCount
+        {
+            get { return unknownExtensions.Count; }
+        }
+
+
+        // Cache the loaded copyright banner text.
+        public string[] CopyrightBanner
+        {
+            get;
+            private set;
+        }
+
+        public string[] PreviousCopyrightBanner
+        {
+            get;
+            private set;
+        }
+
+
+        public void Initialize()
+        {
+            Ext = ExpandSemicolons(Ext);
+            SkipExt = ExpandSemicolons(SkipExt);
+            SkipDir = ExpandSemicolons(SkipDir);
+
+            if (string.IsNullOrEmpty(RootDir))
+            {
+                RootDir = Directory.GetCurrentDirectory();
+            }
+
+            CopyrightBanner = File.ReadAllLines(CopyrightFile);
+
+            if (!string.IsNullOrEmpty(PreviousCopyright))
+            {
+                PreviousCopyrightBanner = File.ReadAllLines(PreviousCopyright);
+            }
+        }
+
+
+        public bool WantDirectory(string dirname)
+        {
+            string dir = Path.GetFileName(dirname);
+
+            return !SkipDir.Contains(dir, StringComparer.OrdinalIgnoreCase);
+        }
+
+
+        public bool WantFile(string filename)
+        {
+            string ext = Path.GetExtension(filename).TrimStart('.');
+
+            // Want it?
+            if (Ext.Contains(ext, StringComparer.OrdinalIgnoreCase))
+            {
+                if (Verbose)
+                    Console.WriteLine("Checking {0}", GetRelativeName(filename));
+
+                return true;
+            }
+
+            // Skip it?
+            if (SkipExt.Contains(ext, StringComparer.OrdinalIgnoreCase))
+            {
+                if (Verbose)
+                    Console.WriteLine("Skipping {0}", GetRelativeName(filename));
+
+                return false;
+            }
+
+            // Warn about it?
+            if (!unknownExtensions.Contains(ext, StringComparer.OrdinalIgnoreCase))
+            {
+                Console.WriteLine("Warning: unknown extension .{0} ({1})", ext, GetRelativeName(filename));
+
+                unknownExtensions.Add(ext);
+            }
+
+            return false;
+        }
+
+
+        public string GetRelativeName(string filename)
+        {
+            if (filename.StartsWith(RootDir, StringComparison.OrdinalIgnoreCase))
+            {
+                return filename.Substring(RootDir.Length).TrimStart('\\', '/');
+            }
+             
+            return filename;
+        }
+
+
+        // Replace ("a;b", "c") -> ("a", "b", "c").
+        static List<string> ExpandSemicolons(List<string> strings)
+        {
+            return strings.Select(s => s.Split(';'))
+                          .SelectMany(s => s)
+                          .ToList();
+        }
+    }
+}
---
+++ tools/copyright/Program.cs
@@ -0,0 +1,141 @@
+// Copyright (c) Microsoft Corporation. All rights reserved.
+//
+// Licensed under the Apache License, Version 2.0 (the "License"); you may
+// not use these files except in compliance with the License. You may obtain
+// a copy of the License at http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+// WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+// License for the specific language governing permissions and limitations
+// under the License.
+
+using System;
+using System.IO;
+using System.Linq;
+
+namespace Copyright
+{
+    class Program
+    {
+        [STAThread]
+        static int Main(string[] args)
+        {
+            // Parse commandline options.
+            var options = new CommandLineOptions();
+            var parser = new CommandLineParser(options);
+
+            if (!parser.ParseCommandLine(args))
+            {
+                return 1;
+            }
+
+            // Run the program logic.
+            try
+            {
+                Run(options);
+            }
+            catch (Exception e)
+            {
+                Console.Error.WriteLine("Error: {0}", e.Message);
+                return 1;
+            }
+
+            return 0;
+        }
+
+
+        static void Run(CommandLineOptions options)
+        {
+            options.Initialize();
+
+            // Recurse through the directory tree.
+            int badFileCount = CheckDirectory(options.RootDir, options);
+
+            // Report errors if anything went wrong.
+            if (options.Validate && badFileCount > 0)
+            {
+                throw new Exception(string.Format("Files with wrong copyright banner: {0}", badFileCount));
+            }
+
+            if (options.UnknownExtensionCount > 0)
+            {
+                throw new Exception(string.Format("Encountered unknown file extensions: {0}", options.UnknownExtensionCount));
+            }
+        }
+
+
+        static int CheckDirectory(string dirname, CommandLineOptions options)
+        {
+            int badFileCount = 0;
+
+            // Recurse into subdirectories.
+            foreach (var subdir in Directory.GetDirectories(dirname))
+            { 
+                if (options.WantDirectory(subdir))
+                {
+                    badFileCount += CheckDirectory(subdir, options);
+                }
+            }
+
+            // Check files.
+            foreach (var filename in Directory.GetFiles(dirname))
+            { 
+                if (options.WantFile(filename))
+                {
+                    badFileCount += CheckFile(filename, options);
+                }
+            }
+
+            return badFileCount;
+        }
+
+
+        static int CheckFile(string filename, CommandLineOptions options)
+        {
+            var fileContents = File.ReadAllLines(filename);
+
+            if (Enumerable.SequenceEqual(options.CopyrightBanner, fileContents.Take(options.CopyrightBanner.Length)))
+            {
+                // This file already has the correct copyright.
+                return 0;
+            }
+            else
+            {
+                if (options.Validate)
+                {
+                    // Just report that the copyright does not match.
+                    Console.WriteLine("Wrong copyright: {0}", options.GetRelativeName(filename));
+                }
+                else
+                {
+                    // Find any existing copyright.
+                    var existingComment = fileContents.TakeWhile(line => line.TrimStart().StartsWith("//"));
+                    var existingBlanks = fileContents.Skip(existingComment.Count()).TakeWhile(line => line.All(char.IsWhiteSpace));
+                    var existingCopyright = existingComment.Concat(existingBlanks);
+
+                    if (options.PreviousCopyrightBanner != null && !Enumerable.SequenceEqual(existingCopyright, options.PreviousCopyrightBanner))
+                    {
+                        // Warn if what we tried to overwrite doesn't match the expected previous (then don't actually edit anything).
+                        Console.WriteLine("Warning: skipping {0} - doesn't match previous copyright", options.GetRelativeName(filename));
+                    }
+                    else
+                    {
+                        // Remove the old copyright.
+                        var withoutOldCopyright = fileContents.Skip(existingCopyright.Count());
+
+                        // Insert the new copyright.
+                        var withNewCopyright = options.CopyrightBanner.Concat(withoutOldCopyright);
+
+                        // Write out the new file.
+                        Console.WriteLine("Updating copyright: {0}", options.GetRelativeName(filename));
+
+                        File.WriteAllLines(filename, withNewCopyright);
+                    }
+                }
+
+                return 1;
+            }
+        }
+    }
+}
