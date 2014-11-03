46e62343551da0104416fda45a2b68cfc4c19e44
Shawn Hargreaves <shawnhar@microsoft.com>
2014-11-03 09:49:28 -0800

    Point at new build server

--- update.bat
+++ update.bat
@@ -1,2 +1,2 @@
-robocopy \\grfxaltbld1\Win2D\docs . /mir /np /nc /ns /nfl /ndl /njh /njs /xf update.bat /xd .git
+robocopy \\grfxaltbld2\Win2D\docs . /mir /np /nc /ns /nfl /ndl /njh /njs /xf update.bat /xd .git
 git add --all .
