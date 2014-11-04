7e61016ed979a2932ac2e5c2244ae9acbf08743e
Shawn Hargreaves <shawnhar@microsoft.com>
2014-11-04 09:41:13 -0800

    Support copying docs from arbitrary builds

--- update.bat
+++ update.bat
@@ -1,2 +1,11 @@
-robocopy \\grfxaltbld2\Win2D\docs . /mir /np /nc /ns /nfl /ndl /njh /njs /xf update.bat /xd .git
+@echo off
+
+if not "%1" == "" (
+    set DOCDIR=%1
+) else (
+    set DOCDIR=\\grfxaltbld2\Win2D
+)
+
+robocopy %DOCDIR%\docs . /mir /np /nc /ns /nfl /ndl /njh /njs /xf update.bat /xd .git
+
 git add --all .
