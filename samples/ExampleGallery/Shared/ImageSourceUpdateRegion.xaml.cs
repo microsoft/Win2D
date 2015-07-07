a9d9f07489d932c4946320d1a4f5de8acb68f02f
Damyan Pepper <damyanp@microsoft.com>
2015-07-07 15:07:09 -0700

    ImageSourceUpdateRegion can now handle lost device itself without using a hidden CanvasControl

--- samples/ExampleGallery/Shared/ImageSourceUpdateRegion.xaml.cs
+++ samples/ExampleGallery/Shared/ImageSourceUpdateRegion.xaml.cs
@@ -2,6 +2,7 @@
 //
 // Licensed under the MIT License. See LICENSE.txt in the project root for license information.
 
+using Microsoft.Graphics.Canvas;
 using Microsoft.Graphics.Canvas.Text;
 using Microsoft.Graphics.Canvas.UI.Xaml;
 using System;
@@ -9,8 +10,7 @@ using Windows.Foundation;
 using Windows.UI;
 using Windows.UI.Xaml;
 using Windows.UI.Xaml.Controls;
-
-// The User Control item template is documented at http://go.microsoft.com/fwlink/?LinkId=234236
+using Windows.UI.Xaml.Media;
 
 namespace ExampleGallery
 {
@@ -19,7 +19,7 @@ namespace ExampleGallery
         int width = 1024;
         int height = 1024;
         int step = 32;
-        CanvasImageSource imageSource;
+        CanvasImageSource imageSource = null;
         DispatcherTimer dispatcherTimer = new DispatcherTimer();
         Random random = new Random();
 
@@ -27,36 +27,73 @@ namespace ExampleGallery
         {
             this.InitializeComponent();
 
-            dispatcherTimer.Interval = new TimeSpan(0, 0, 0, 0, 250);
+            dispatcherTimer.Interval = TimeSpan.FromSeconds(0.25);
             dispatcherTimer.Tick += OnTick;
             dispatcherTimer.Start();
         }
 
-        private void UserControl_Unloaded(object sender, RoutedEventArgs e)
+        private void UserControl_Loaded(object sender, RoutedEventArgs args)
+        {
+            CompositionTarget.SurfaceContentsLost += CompositionTarget_SurfaceContentsLost;
+            UpdateImageSource();
+        }
+
+        private void UserControl_Unloaded(object sender, RoutedEventArgs args)
         {
             dispatcherTimer.Stop();
-
-            // Explicitly remove references to allow the Win2D controls to get garbage collected
-            canvas.RemoveFromVisualTree();
-            canvas = null;
+            CompositionTarget.SurfaceContentsLost -= CompositionTarget_SurfaceContentsLost;
         }
 
-        private void OnTick(object sender, object e)
+        private void OnTick(object sender, object args)
         {
-            int updateWidth = random.Next(step, Math.Min(width - step, step * 5));
-            int updateHeight = random.Next(step, Math.Min(height - step, step * 5));
-            int x = random.Next(0, width - updateWidth);
-            int y = random.Next(0, height - updateHeight);
-
-            byte[] color = new byte[3];
-            random.NextBytes(color);
-
-            Draw(x, y, updateWidth, updateHeight, Color.FromArgb(255, color[0], color[1], color[2]));
+            UpdateImageSource();
         }
 
-        private void OnCreateResources(CanvasControl sender, object args)
+        private void CompositionTarget_SurfaceContentsLost(object sender, object e)
         {
-            imageSource = new CanvasImageSource(sender, width, height);
+            imageSource = null;
+            UpdateImageSource();
+        }
+
+        private void UpdateImageSource()
+        {
+            try
+            {
+                if (imageSource == null)
+                {
+                    CreateImageSource();
+                }
+                else
+                {
+                    int updateWidth = random.Next(step, Math.Min(width - step, step * 5));
+                    int updateHeight = random.Next(step, Math.Min(height - step, step * 5));
+                    int x = random.Next(0, width - updateWidth);
+                    int y = random.Next(0, height - updateHeight);
+
+                    byte[] color = new byte[3];
+                    random.NextBytes(color);
+
+                    Draw(x, y, updateWidth, updateHeight, Color.FromArgb(255, color[0], color[1], color[2]));
+                }
+            }
+            catch (Exception e)
+            {
+                if (imageSource != null && imageSource.Device.IsDeviceLost(e.HResult))
+                {
+                    // XAML will also raise a SurfaceContentsLost event, and we use this to trigger redrawing
+                    // the surface.
+                }
+                else
+                {
+                    throw;
+                }
+            }
+        }
+
+        private void CreateImageSource()
+        {
+            var device = CanvasDevice.GetSharedDevice(false);
+            imageSource = new CanvasImageSource(device, width, height, 96);
             image.Source = imageSource;
 
             Draw(0, 0, width, height, Colors.Blue);
@@ -64,53 +101,33 @@ namespace ExampleGallery
 
         private void Draw(int updateLeft, int updateTop, int updateWidth, int updateHeight, Color color)
         {
-            if (imageSource == null)
-                return;
-
             int drawTextCalls = 0;
 
-            try
+            using (var ds = imageSource.CreateDrawingSession(color, new Rect(updateLeft, updateTop, updateWidth, updateHeight)))
             {
-                using (var ds = imageSource.CreateDrawingSession(color, new Rect(updateLeft, updateTop, updateWidth, updateHeight)))
+                CanvasTextFormat format = new CanvasTextFormat
                 {
-                    CanvasTextFormat format = new CanvasTextFormat
+                    FontSize = 16,
+                    HorizontalAlignment = CanvasHorizontalAlignment.Center,
+                    VerticalAlignment = CanvasVerticalAlignment.Center
+                };
+
+                // Round update region to step
+                updateLeft = (updateLeft / step) * step;
+                updateTop = (updateTop / step) * step;
+                int updateRight = updateLeft + ((updateWidth / step) + 1) * step;
+                int updateBottom = updateTop + ((updateHeight / step) + 1) * step;
+
+                for (int x = updateLeft; x <= updateRight; x += step)
+                {
+                    for (int y = updateTop; y <= updateBottom; y += step)
                     {
-                        FontSize = 16,
-                        HorizontalAlignment = CanvasHorizontalAlignment.Center,
-                        VerticalAlignment = CanvasVerticalAlignment.Center
-                    };
+                        int n = (x / step) + (y / step) * width;
+                        var str = (n % 99).ToString();
 
-                    // Round update region to step
-                    updateLeft = (updateLeft / step) * step;
-                    updateTop = (updateTop / step) * step;
-                    int updateRight = updateLeft + ((updateWidth / step) + 1) * step;
-                    int updateBottom = updateTop + ((updateHeight / step) + 1) * step;
-
-                    for (int x = updateLeft; x <= updateRight; x += step)
-                    {
-                        for (int y = updateTop; y <= updateBottom; y += step)
-                        {
-                            int n = (x / step) + (y / step) * width;
-                            var str = (n % 99).ToString();
-
-                            ds.DrawText(str, x, y, Colors.White, format);
-                            drawTextCalls++;
-                        }
+                        ds.DrawText(str, x, y, Colors.White, format);
+                        drawTextCalls++;
                     }
-                }
-            }
-            catch (System.Runtime.InteropServices.COMException e)
-            {
-                // Ignore device lost errors; ultimately, the control will be notified that surfaces have been lost and will call OnCreateResources
-                switch ((uint)e.HResult)
-                {
-                    case 0x887A0006: // DXGI_ERROR_DEVICE_HUNG
-                    case 0x887A0005: // DXGI_ERROR_DEVICE_REMOVED
-                    case 0x887A0007: // DXGI_ERROR_DEVICE_RESET
-                    case 0x887A0020: // DXGI_ERROR_DRIVER_INTERNAL_ERROR
-                        break;
-                    default:
-                        throw;
                 }
             }
 
