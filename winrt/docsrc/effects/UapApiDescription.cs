07eb9d96c11f0f181c43514647af6ecee643eaab
Shawn Hargreaves <shawnhar@microsoft.com>
2015-07-06 20:58:21 -0700

    Change license from Apache 2.0 to MIT - autogenerated updates to header comments

--- winrt/docsrc/effects/UapApiDescription.cs
+++ winrt/docsrc/effects/UapApiDescription.cs
@@ -1,14 +1,6 @@
 // Copyright (c) Microsoft Corporation. All rights reserved.
 //
-// Licensed under the Apache License, Version 2.0 (the "License"); you may
-// not use these files except in compliance with the License. You may obtain
-// a copy of the License at http://www.apache.org/licenses/LICENSE-2.0
-//
-// Unless required by applicable law or agreed to in writing, software
-// distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
-// WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
-// License for the specific language governing permissions and limitations
-// under the License.
+// Licensed under the MIT License. See LICENSE.txt in the project root for license information.
 
 // TODO #3356 - delete this file when we switch docs to have UAP as primary focus.
 // It is needed because our docs are currently generated against the 8.1 flavor of Win2D, 
