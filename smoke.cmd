7178eba1ee8a98f0c583eae1d8724f14f72107b1
Shawn Hargreaves <shawnhar@microsoft.com>
2015-09-14 17:36:18 -0700

    smoke.cmd should display MSBuild error summary at the end of the test run

--- smoke.cmd
+++ smoke.cmd
@@ -1,3 +1,3 @@
 @echo off
 
-msbuild "%~dp0Win2D.proj" /v:m /maxcpucount /nr:false /p:BuildPlatforms="x64;AnyCPU" /p:BuildConfigurations=Debug /p:BuildPhone=false /p:BuildDocs=false %*
+msbuild "%~dp0Win2D.proj" /v:n /maxcpucount /nr:false /p:BuildPlatforms="x64;AnyCPU" /p:BuildConfigurations=Debug /p:BuildPhone=false /p:BuildDocs=false %*
