0c50eaa0f668074ad0a0490bc1e05535fa6afec6
Shawn Hargreaves <shawnhar@microsoft.com>
2015-09-16 23:13:03 -0700

    Upgrade NuGet download script to pull the 3.2 RTM version of nuget.exe

--- build/nuget/download-nuget.cmd
+++ build/nuget/download-nuget.cmd
@@ -1,1 +1,1 @@
-powershell -command "(New-Object System.Net.WebClient).DownloadFile('http://nuget.org/nuget.exe', '%~pd0nuget.exe')"
+powershell -command "(New-Object System.Net.WebClient).DownloadFile('http://dist.nuget.org/win-x86-commandline/latest/nuget.exe', '%~pd0nuget.exe')"
