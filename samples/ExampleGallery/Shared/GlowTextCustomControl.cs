8965e6f482763465fdf643c825ee1ab586d769b8
Meinrad Jean-Richard <mjr@jean-richard.ch>
2016-08-10 19:25:26 +0200

    Fixed #373: Added Measure() call to the canvas control. (#406)

--- samples/ExampleGallery/Shared/GlowTextCustomControl.cs
+++ samples/ExampleGallery/Shared/GlowTextCustomControl.cs
@@ -121,7 +121,7 @@ namespace ExampleGallery
             {
                 instance.canvas.Invalidate();
                 instance.InvalidateMeasure();
-            }            
+            }
         }
 
         // This is the amount that we grow the desired size by (to account for the glow)
@@ -142,7 +142,11 @@ namespace ExampleGallery
             var layout = CreateTextLayout(device, availableSize);
             var bounds = layout.LayoutBounds;
 
-            return new Size(Math.Min(availableSize.Width, bounds.Width + ExpandAmount), Math.Min(availableSize.Height, bounds.Height + ExpandAmount));
+            Size desiredSize = new Size(Math.Min(availableSize.Width, bounds.Width + ExpandAmount), Math.Min(availableSize.Height, bounds.Height + ExpandAmount));
+            if (canvas != null)
+                canvas.Measure(desiredSize);
+
+            return desiredSize;
         }
 
         private void OnDraw(CanvasControl sender, CanvasDrawEventArgs args)
