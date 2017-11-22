261dcbab47a70e3d4e5db302bd64799d01024307
Andy Dadi <adadi@microsoft.com>
2017-11-22 08:05:05 -0800

    RS4 SDK, fix metadata

--- winrt/inc/GeometryInterop.h
+++ winrt/inc/GeometryInterop.h
@@ -4,46 +4,16 @@
 
 #pragma once
 
-#ifdef NTDDI_WIN10_RS3 //NTDDI_WIN10_RS4
-#if NTDDI_VERSION >= NTDDI_WIN10_RS3 //NTDDI_WIN10_RS4
+#ifdef NTDDI_WIN10_RS4
+#if NTDDI_VERSION >= NTDDI_WIN10_RS4
 #define ENABLE_GEOMETRY_INTEROP 1
-
-// Until the RS4 SDK is available
-#define NEED_TO_DEFINE_GEOMETRY_INTEROP_INTERFACES 1
-
 #endif
 #endif
+
+#define ENABLE_GEOMETRY_INTEROP 1
 
 #ifndef __midl
 #ifdef ENABLE_GEOMETRY_INTEROP
-#ifndef NEED_TO_DEFINE_GEOMETRY_INTEROP_INTERFACES
-
 #include "Windows.Graphics.Interop.h"
-
-#else
-
-namespace ABI {
-    namespace Windows {
-        namespace Graphics
-        {
-#undef INTERFACE
-#define INTERFACE IGeometrySource2DInterop
-            DECLARE_INTERFACE_IID_(IGeometrySource2DInterop, IUnknown, "0657AF73-53FD-47CF-84FF-C8492D2A80A3")
-            {
-                IFACEMETHOD(GetGeometry)(
-                    _COM_Outptr_ ID2D1Geometry** value
-                    ) PURE;
-
-                IFACEMETHOD(TryGetGeometryUsingFactory)(
-                    _In_ ID2D1Factory* factory,
-                    _COM_Outptr_result_maybenull_ ID2D1Geometry** value
-                    ) PURE;
-            };
-        }
-    }
-}
-
 #endif
 #endif
-#endif
-
