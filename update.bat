8ed471dbbaeaa99fe07191f760d2c90d4fe55eda
Shawn Hargreaves <shawnhar@microsoft.com>
2018-03-19 13:35:36 -0700

    Update update.bat to reflect new build system

--- update.bat
+++ update.bat
@@ -1,11 +1,19 @@
 @echo off
 
-if not "%1" == "" (
-    set DOCDIR=%1
-) else (
-    set DOCDIR=\\grfxaltbld2\Win2D
+if "%1" == "" (
+    echo Usage: update {buildname}  // eg: update 1803.16018
+    goto END
 )
 
-robocopy %DOCDIR%\docs . /mir /np /nc /ns /nfl /ndl /njh /njs /xf update.bat /xd .git
+set DOCDIR=\\edge-svcs\Release\Win2D\Win2D-release\%1\bin\docs
+
+if not exist %DOCDIR% (
+    echo Not found: %DOCDIR%
+    goto END
+)
+
+robocopy %DOCDIR% . /mir /np /nc /ns /nfl /ndl /njh /njs /xf update.bat /xd .git
 
 git add --all .
+
+:END
