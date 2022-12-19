8e3d6745bbf5e882410d4c0c5f42eb4f923d797e
Geoffrey Trousdale <getrou@microsoft.com>
2022-12-19 16:44:04 +0000

    Merged PR 23667: Update clear-space to not remove githash
    The clear-space script is used to clean the build and prevent the build machine from running out of space between building and post-build file copies. This change updates the clear-space script to not remove githash, which is required for the release process.

--- build/clear-space.cmd
+++ build/clear-space.cmd
@@ -1,5 +1,11 @@
 dir /s
 
 del /f /s /q build\pkges
+
+@REM Keep the githash from the obj folder since the nuspec requires it
+copy obj\Win2d.githash.txt Win2d.githash.txt
 del /f /s /q obj
+mkdir obj
+move Win2d.githash.txt obj\Win2d.githash.txt
+
 del /f /s /q .git
