b0fa0e367ced30d13cb2f7c14af1f4f37bbc9a4f
benstevens48 <ben@stevens.me.uk>
2026-02-25 09:32:59 +0000

    remove missing effects

--- winrt/lib/effects/generated/EffectMakers.cpp
+++ winrt/lib/effects/generated/EffectMakers.cpp
@@ -33,7 +33,6 @@
 #include "GammaTransferEffect.h"
 #include "GaussianBlurEffect.h"
 #include "GrayscaleEffect.h"
-#include "HdrToneMapEffect.h"
 #include "HighlightsAndShadowsEffect.h"
 #include "HueRotationEffect.h"
 #include "HueToRgbEffect.h"
@@ -66,7 +65,6 @@
 #include "TurbulenceEffect.h"
 #include "UnPremultiplyEffect.h"
 #include "VignetteEffect.h"
-#include "WhiteLevelAdjustmentEffect.h"
 
 
 std::pair<IID, CanvasEffect::MakeEffectFunction> CanvasEffect::m_effectMakers[] =
@@ -98,7 +96,6 @@ std::pair<IID, CanvasEffect::MakeEffectFunction> CanvasEffect::m_effectMakers[] 
     { GammaTransferEffect::EffectId(),        MakeEffect<GammaTransferEffect>        },
     { GaussianBlurEffect::EffectId(),         MakeEffect<GaussianBlurEffect>         },
     { GrayscaleEffect::EffectId(),            MakeEffect<GrayscaleEffect>            },
-    { HdrToneMapEffect::EffectId(),           MakeEffect<HdrToneMapEffect>           },
     { HighlightsAndShadowsEffect::EffectId(), MakeEffect<HighlightsAndShadowsEffect> },
     { HueRotationEffect::EffectId(),          MakeEffect<HueRotationEffect>          },
     { HueToRgbEffect::EffectId(),             MakeEffect<HueToRgbEffect>             },
@@ -131,7 +128,6 @@ std::pair<IID, CanvasEffect::MakeEffectFunction> CanvasEffect::m_effectMakers[] 
     { TurbulenceEffect::EffectId(),           MakeEffect<TurbulenceEffect>           },
     { UnPremultiplyEffect::EffectId(),        MakeEffect<UnPremultiplyEffect>        },
     { VignetteEffect::EffectId(),             MakeEffect<VignetteEffect>             },
-    { WhiteLevelAdjustmentEffect::EffectId(), MakeEffect<WhiteLevelAdjustmentEffect> },
 
     { GUID_NULL, nullptr }
 };
